faf5ce9073270301c842575286627a208b6254c3
Srinjoy <mr.srinjoy@gmail.com>
2024-04-13 16:52:32 +0000

    practice day 4

---
+++ palindrome_String.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class palindrome_String {
+    
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String str=in.nextLine();
+        int start=0;
+        char[] arr=str.toCharArray();
+        int end=arr.length-1;
+        
+        while (start<end) {
+            if (arr[start]!=arr[end]){
+                break;
+            }
+            else {
+                start++;
+                end--;
+            }
+        }
+        System.out.println(start==end);
+        in.close();
+    }
+}
