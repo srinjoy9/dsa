6df0a52f382fca25a17447ead81ed66d5427c8de
Srinjoy <mr.srinjoy@gmail.com>
2024-04-13 17:00:11 +0000

    edit

--- palindrome_String.java
+++ palindrome_String.java
@@ -4,6 +4,7 @@ public class palindrome_String {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         String str=in.nextLine();
+        str=str.toLowerCase();
         int start=0;
         char[] arr=str.toCharArray();
         int end=arr.length-1;
